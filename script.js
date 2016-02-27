04f95776e2351ab1bdd09de72638dd27c62c1f0b
Rohan Varma <rvarm1@gmail.com>
2016-02-27 09:17:13 -0800

    hrioije

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	
 google.charts.load('current', {'packages': ['geochart']});
-     google.charts.setOnLoadCallback(drawMarkersMap);
+     
      google.charts.setOnLoadCallback(drawRegionsMap);
 
       function drawRegionsMap() {
@@ -22,33 +22,6 @@ google.charts.load('current', {'packages': ['geochart']});
 
         chart.draw(data, options);
       }
-
-      function drawMarkersMap() {
-      var data = google.visualization.arrayToDataTable([
-        ['City',   'Population', 'Area'],
-        ['Rome',      2761477,    1285.31],
-        ['Milan',     1324110,    181.76],
-        ['Naples',    959574,     117.27],
-        ['Turin',     907563,     130.17],
-        ['Palermo',   655875,     158.9],
-        ['Genoa',     607906,     243.60],
-        ['Bologna',   380181,     140.7],
-        ['Florence',  371282,     102.41],
-        ['Fiumicino', 67370,      213.44],
-        ['Anzio',     52192,      43.43],
-        ['Ciampino',  38262,      11]
-      ]);
-
-      var t = {
-        region: 'IT',
-        displayMode: 'markers',
-        colorAxis: {colors: ['green', 'blue']}
-      };
-
-      var c = new google.visualization.GeoChart(document.getElementById('chart_div'));
-      c.draw(data, t);
-    };
-
 
 
 });
