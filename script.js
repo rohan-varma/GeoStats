a2a8ef8d33bb384cd621bce1a085f0cb3c1a3ba7
Rohan Varma <rvarm1@gmail.com>
2016-02-27 09:13:06 -0800

    uharue

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	
-
-google.charts.load('current', {'packages':['geochart']});
-      google.charts.setOnLoadCallback(drawRegionsMap);
+google.charts.load('current', {'packages': ['geochart']});
+     google.charts.setOnLoadCallback(drawMarkersMap);
+     google.charts.setOnLoadCallback(drawRegionsMap);
 
       function drawRegionsMap() {
 
         var data = google.visualization.arrayToDataTable([
-          ['Country', 'Popularity', "number of hoes"],
-          ['Germany', 200, 5.4],
-          ['United States', 300, 10],
-          ['Brazil', 400, 89],
-          ['Canada', 500,12],
-          ['France', 600,33],
-          ['RU', 700,1283]
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
         ]);
 
         var options = {};
@@ -22,6 +22,32 @@ google.charts.load('current', {'packages':['geochart']});
 
         chart.draw(data, options);
       }
+
+      function drawMarkersMap() {
+      var data = google.visualization.arrayToDataTable([
+        ['City',   'Population', 'Area'],
+        ['Rome',      2761477,    1285.31],
+        ['Milan',     1324110,    181.76],
+        ['Naples',    959574,     117.27],
+        ['Turin',     907563,     130.17],
+        ['Palermo',   655875,     158.9],
+        ['Genoa',     607906,     243.60],
+        ['Bologna',   380181,     140.7],
+        ['Florence',  371282,     102.41],
+        ['Fiumicino', 67370,      213.44],
+        ['Anzio',     52192,      43.43],
+        ['Ciampino',  38262,      11]
+      ]);
+
+      var t = {
+        region: 'IT',
+        displayMode: 'markers',
+        colorAxis: {colors: ['green', 'blue']}
+      };
+
+      var c = new google.visualization.GeoChart(document.getElementById('chart_div'));
+      c.draw(data, t);
+    };
 
 
 
