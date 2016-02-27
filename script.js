80defd2023cb32881ba0f411668aa441268189d2
Rohan Varma <rvarm1@gmail.com>
2016-02-27 11:00:12 -0800

    hi

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-	
+
+	$(".button").click(function(){
+    $("#timeline").html("yay");
+});
 
 	google.charts.load('current', {'packages':['geochart']});
       google.charts.setOnLoadCallback(drawRegionsMap);
@@ -7,21 +10,21 @@ $(document).ready(function(){
       function drawRegionsMap() {
 
         var data = google.visualization.arrayToDataTable([
-          ['Country', 'Popularity', "number of hoes"],
-          ['Germany', 200, 5.4],
-          ['United States', 300, 10],
-          ['Brazil', 400, 89],
-          ['Canada', 500,12],
-          ['France', 600,33],
-          ['RU', 700,1283]
+          ['country', 'Popularity', "number of hoes"],
+          ['united states', 200, 5.4],
+          ['Canada', 300, 10],
+          ['Germany', 400, 89],
+          ['India', 500,12],
+          ['China', 600,33],
+          ['South Africa', 700,1283]
         ]);
 
         var options = {
-          region: '019', // Africa
-          colorAxis: {colors: ['#00853f', 'black', '#e31b23']},
-          backgroundColor: '#81d4fa',
-          datalessRegionColor: '#f8bbd0',
-          defaultColor: '#f5f5f5',
+         // region: '019', 
+         // colorAxis: {colors: ['#00853f', 'black', '#e31b23']},
+         // backgroundColor: '#81d4fa',
+          //datalessRegionColor: '#f8bbd0',
+          //defaultColor: '#f5f5f5',
         };
 
         var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
